f856431ee48277933d9445ab384e2a72eb2ddbec
Hikapasya05 <100111267+Hikapasya05@users.noreply.github.com>
2023-09-14 11:53:32 +0700

    login xml -hika

---
+++ app/src/main/java/com/example/mytsuclassroom/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.mytsuclassroom.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String,
+)
