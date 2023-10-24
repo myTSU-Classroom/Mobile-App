33bb3278010aaef13c27cff4ff7f2bf0fb1ef9bc
Hikapasya05 <100111267+Hikapasya05@users.noreply.github.com>
2023-10-24 18:18:05 +0700

    Create Group and Education Direction XML
    layouts for Group and Education Direction Mobile App

--- app/src/main/java/com/example/mytsuclassroom/AddNewGroupFragment.kt
+++ app/src/main/java/com/example/mytsuclassroom/AddNewGroupFragment.kt
@@ -1,11 +1,10 @@
-package com.example.mytsuclassroom.ui.adminpage
+package com.example.mytsuclassroom
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.mytsuclassroom.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AdministrationFragment.newInstance] factory method to
+ * Use the [AddNewGroupFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AdministrationFragment : Fragment() {
+class AddNewGroupFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +34,7 @@ class AdministrationFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_administration, container, false)
+        return inflater.inflate(R.layout.fragment_add_new_group, container, false)
     }
 
     companion object {
@@ -45,12 +44,12 @@ class AdministrationFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AdministrationFragment.
+         * @return A new instance of fragment AddNewGroupFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AdministrationFragment().apply {
+            AddNewGroupFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
